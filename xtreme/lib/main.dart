29223f34e0d7ca26ff5cff12f4a13eee80c49b53
“Abdullah-Abuzaid” <bozaida@gmail.com>
2023-11-29 21:17:34 +0300

    add firebase

--- xtreme/lib/main.dart
+++ xtreme/lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyDqqC6er8Cmr2IFv9HlBihUOk66EQVWsW0",
+          authDomain: "xtreme-flutter-weekend.firebaseapp.com",
+          projectId: "xtreme-flutter-weekend",
+          storageBucket: "xtreme-flutter-weekend.appspot.com",
+          messagingSenderId: "825306918811",
+          appId: "1:825306918811:web:9376181dd603f36cca7b3e"));
+
   runApp(const MyApp());
 }
 
@@ -82,27 +93,11 @@ class _MyHomePageState extends State<MyHomePage> {
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
