2d4a47e9b3267d051040cd185ff50d24c42a87cb
Sameer <samiralsabea@gmail.com>
2023-12-01 19:04:19 +0300

    Commit Changes in the page

--- lib/home.dart
+++ lib/home.dart
@@ -13,59 +13,132 @@ class _homePageState extends State<homePage> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        bottomNavigationBar: bottomNev(),
         backgroundColor: const Color(0xFF007d40),
         body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  child: Image.asset(
-                    'assets/images/kfupm.png',
-                    height: 80,
-                    width: 80,
-                    alignment: Alignment.center,
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Column(
                   children: [
-                    const Text(
-                      "Hi Sameer!",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold),
-                    ), //Text here is the name of the user
                     Container(
-                      height: 50,
-                      width: 50,
-                      decoration: BoxDecoration(
-                        color: Colors.green[500],
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: const Icon(
-                        Icons.notifications,
-                        color: Colors.white,
-                        size: 30,
+                      alignment: Alignment.center,
+                      child: Image.asset(
+                        'assets/images/kfupm.png',
+                        height: 60,
+                        width: 80,
+                        alignment: Alignment.center,
                       ),
                     ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          "Hi Sameer!",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold),
+                        ), //Text here is the name of the user
+                        Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.green[500],
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: const Icon(
+                            Icons.notifications,
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: const Text(
+                        "12 Dec 2023 ",
+                        style: TextStyle(
+                          color: Colors.white70,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+
+                    SizedBox(
+                      height: 20,
+                    ),
+                    // Search Bar
+                    SearchBar(),
                   ],
                 ),
-                const Text(
-                  "12 Dec 2023 ",
-                  style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 12,
-                  ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Expanded(
+                  child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                    topRight: Radius.circular(50)),
+                child: Container(
+                  color: Colors.grey[100],
                 ),
-              ],
-            ),
+              ))
+            ],
           ),
         ),
       ),
     );
   }
+
+  Container SearchBar() {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white70,
+        borderRadius: BorderRadius.all(
+          Radius.circular(12),
+        ),
+      ),
+      padding: const EdgeInsets.all(12),
+      child: Row(
+        children: [
+          Icon(
+            Icons.search,
+            color: Colors.white,
+          ),
+          const SizedBox(
+            width: 5,
+          ),
+          Text(
+            "Search",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+BottomNavigationBar bottomNev() {
+  return BottomNavigationBar(
+    items: [
+      BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        label: "Home",
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.person),
+        label: "Profile",
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.settings),
+        label: "Settings",
+      ),
+    ],
+  );
 }
