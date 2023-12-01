ab33d7dbb37858fe9e0eaa75b4605df8726ac158
Reda Al Sulais <u.yokozuna@gmail.com>
2023-12-01 15:27:26 +0300

    create club event model and api

---
+++ lib/club_event/club_event.dart
@@ -0,0 +1,101 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ClubEvent {
+  Timestamp date;
+  String description;
+  String icon;
+  String organizer;
+  GeoPoint place;
+  String registrationLink;
+  String title;
+
+  ClubEvent({
+    required this.date,
+    required this.description,
+    required this.icon,
+    required this.organizer,
+    required this.place,
+    required this.registrationLink,
+    required this.title,
+  });
+
+  ClubEvent copyWith({
+    Timestamp? date,
+    String? description,
+    String? icon,
+    String? organizer,
+    GeoPoint? place,
+    String? registrationLink,
+    String? title,
+  }) {
+    return ClubEvent(
+      date: date ?? this.date,
+      description: description ?? this.description,
+      icon: icon ?? this.icon,
+      organizer: organizer ?? this.organizer,
+      place: place ?? this.place,
+      registrationLink: registrationLink ?? this.registrationLink,
+      title: title ?? this.title,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'date': date,
+      'description': description,
+      'icon': icon,
+      'organizer': organizer,
+      'place': place,
+      'registrationLink': registrationLink,
+      'title': title,
+    };
+  }
+
+  factory ClubEvent.fromMap(Map<String, dynamic> map) {
+    return ClubEvent(
+      date: map['date'] as Timestamp, // TODO:
+      description: map['description'] as String,
+      icon: map['icon'] as String,
+      organizer: map['organizer'] as String,
+      place: map['place'] as GeoPoint, // TODO:
+      registrationLink: map['registrationLink'] as String,
+      title: map['title'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ClubEvent.fromJson(String source) =>
+      ClubEvent.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'ClubEvent(date: $date, description: $description, icon: $icon, organizer: $organizer, place: $place, registrationLink: $registrationLink, title: $title)';
+  }
+
+  @override
+  bool operator ==(covariant ClubEvent other) {
+    if (identical(this, other)) return true;
+
+    return other.date == date &&
+        other.description == description &&
+        other.icon == icon &&
+        other.organizer == organizer &&
+        other.place == place &&
+        other.registrationLink == registrationLink &&
+        other.title == title;
+  }
+
+  @override
+  int get hashCode {
+    return date.hashCode ^
+        description.hashCode ^
+        icon.hashCode ^
+        organizer.hashCode ^
+        place.hashCode ^
+        registrationLink.hashCode ^
+        title.hashCode;
+  }
+}
